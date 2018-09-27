efa470f21b2eb6577762102aba9ffbdcfd049a33
Adam Sarkany <adam.sarkany@atos.net>
2018-09-27 13:08:58 +0200

    new deployment

--- bc-demo-with-react/ethereum/factory.js
+++ bc-demo-with-react/ethereum/factory.js
@@ -3,7 +3,7 @@ import SupplyFactory from './build/SupplyChainFactory.json';
 
 const instance  = new web3.eth.Contract(
   JSON.parse(SupplyFactory.interface),
-  '0xa65afe5826579E9D2CE9024cB422618aFcD0D87D'
+  '0xE228DF0064904f4d1E937071831f4cc8F4c86331'
 );
 
 export default instance;
